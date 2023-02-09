82431b077312f3a9d1c97498954dd35c0a04344a
Khanmag <Khanmag329@gmail.com>
2023-02-09 20:40:29 +0300

    second lesson

---
+++ script.js
@@ -0,0 +1,74 @@
+// var c = 15
+// let a = "seven"
+// a = 7
+// const b = 8
+
+
+//STRING
+// const str1 = 'I\'m OK'
+// const str2 = "Саша \nсказал: \"Привет\""
+// const str3 = `I'm say ${str1}`
+// console.log(str1)
+// console.log(str2)
+// console.log(str3)
+
+//NUMBER
+// + - * / **
+// const num1 = 75 + 15
+// const num2 = 79**3
+// const rem = 8 * "a"
+//  NaN not a number
+// const inf = 17 / 0
+// console.log(rem)
+// alert(num2)
+// от -(2**53 -1) до (2**53 -1)
+// console.log(999999999999999999999999999999)
+
+//BIGINT
+// const bigint = 1031231233124234213214123n
+// console.log(bigint)
+
+//BOOLEAN
+// true / false
+// > < >= <= >== <== == ===
+// const bool = "a" > "8"
+// console.log(bool)
+
+
+//NULL
+// let empty = null
+// console.log(empty, typeof empty)
+
+//UNDEFINED
+// let box
+// console.log(box, typeof box)
+
+//SYMBOL
+// const uniq = Symbol('id')
+// const uniq2 = Symbol('id')
+// console.log(uniq == uniq2)
+
+//OBJECT
+
+const obj = {
+    name: "Sasha",
+    age: 19,
+    isHappy: true,
+}
+// console.log(obj.name)
+// console.log(obj['name'])
+
+obj.job = "Google"
+
+const array = ["Аня", 18, false]
+array[3] = "Facebook"
+console.log(array)
+console.log(array[0])
+
+
+
+
+
+
+
+
