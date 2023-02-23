46068e2e33f81263ad4932c7d1f74b0b9f9987a1
Shamil <khanmag329@gmail.com>
2023-02-23 11:32:55 +0300

    init

--- script.js
+++ script.js
@@ -1,69 +1,105 @@
-// var c = 15
-// let a = "seven"
-// a = 7
-// const b = 8
 
 
-//STRING
-// const str1 = 'I\'m OK'
-// const str2 = "Саша \nсказал: \"Привет\""
-// const str3 = `I'm say ${str1}`
-// console.log(str1)
-// console.log(str2)
-// console.log(str3)
 
-//NUMBER
-// + - * / **
-// const num1 = 75 + 15
-// const num2 = 79**3
-// const rem = 8 * "a"
-//  NaN not a number
-// const inf = 17 / 0
-// console.log(rem)
-// alert(num2)
-// от -(2**53 -1) до (2**53 -1)
-// console.log(999999999999999999999999999999)
-
-//BIGINT
-// const bigint = 1031231233124234213214123n
-// console.log(bigint)
-
-//BOOLEAN
-// true / false
-// > < >= <= >== <== == ===
-// const bool = "a" > "8"
-// console.log(bool)
-
-
-//NULL
-// let empty = null
-// console.log(empty, typeof empty)
-
-//UNDEFINED
-// let box
-// console.log(box, typeof box)
-
-//SYMBOL
-// const uniq = Symbol('id')
-// const uniq2 = Symbol('id')
-// console.log(uniq == uniq2)
-
-//OBJECT
-
-const obj = {
-    name: "Sasha",
-    age: 19,
-    isHappy: true,
+const getRandomNumInRange = (min, max) => {
+    const randomNum = (Math.random() * (max - min) + min ).toFixed(0)
+    return randomNum
 }
-// console.log(obj.name)
-// console.log(obj['name'])
 
-obj.job = "Google"
+const getTask = () => {
+    const symbol = (Math.random() > 0.5) ? "+" : "-"
+    const task = `${getRandomNumInRange(0, 100)} ${symbol} ${getRandomNumInRange(0, 100)}`
+    gameState.rightAnswer = eval(task)
+    return task
+}
 
-const array = ["Аня", 18, false]
-array[3] = "Facebook"
-console.log(array)
-console.log(array[0])
+const toggleGameState = () => {
+    gameState.taskInProcess = !gameState.taskInProcess
+}
+
+
+
+
+const gameElements = document.getElementById("my_game").children
+const title = gameElements[0]
+const userTask = gameElements[1]
+const userAnswer = gameElements[2]
+const btnGame = gameElements[3]
+
+const gameState = {
+    taskInProcess: false,
+    rightAnswer: null,
+}
+
+const startGameFunc = () => {
+    if (!gameState.taskInProcess) {
+        title.innerText = "Игра началась!"
+        userAnswer.value = null
+        userTask.innerText = getTask()
+        userAnswer.hidden = false
+    } else {
+        const isRight = gameState.rightAnswer == userAnswer.value
+        userTask.innerText = userTask.innerText + " = " + gameState.rightAnswer
+        title.innerText = "Вы п" + ((isRight) ? "обедили!" : "роиграли!")
+    }
+    toggleGameState()
+    btnGame.innerText = (gameState.taskInProcess) ? "Проверить!" : "Начать заново!"
+}
+btnGame.addEventListener("click", startGameFunc)
+userAnswer.addEventListener("keydown", (e) => {
+    // console.log(e)
+    if (e.key === "Enter") {
+        startGameFunc()
+    } else if (e.key === "Escape") {
+        userAnswer.blur()
+    }
+})
+
+
+
+
+
+
+
+const choosedEl = document.querySelectorAll(".choosed_block-container > div")
+const counterEl = document.querySelector(".choosed_block span")
+
+// const choosedState = {
+//     countElements: 0,
+// }
+// const changeCount = (value) => {
+//     choosedState.countElements += value
+//     counterEl.innerText = choosedState.countElements
+// }
+const choosedState = {
+    countElements: 0,
+    setCountValue(value) {
+            this.countElements += value
+            counterEl.innerText = this.countElements
+    }
+}
+
+const eventFunc = (e) => {
+    // choosedEl[i].className = "choosed_element"
+    if (e.target.className === "") {
+        e.target.className = "choosed_element"
+        choosedState.setCountValue(1)
+    } else {
+        e.target.className = ""
+        choosedState.setCountValue(-1)
+    }
+}
+
+
+for (let i = 0; i < choosedEl.length; i++) {
+    choosedEl[i].addEventListener("click", eventFunc)
+}
+choosedEl[2].removeEventListener("click", eventFunc)
+
+
+
+
+
 
 
 
